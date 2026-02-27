33ee1e434c40d5ec0bb169e19af854436d8b8b3e
mlizanoj <mlizanoj@campusdigitalfp.com>
2026-02-27 12:04:31 +0100

    Empezando pestaña content.php ,y haciendo su parte de CSS

---
+++ content.php
@@ -0,0 +1,52 @@
+<?php
+?>
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="styles/style.css">
+    <title>Bienvenidas</title>
+    <link rel="icon" type="image/png" sizes="32x32" href="https://www.medicosdelmundo.org/app/themes/mdm/library/medias/favicon/favicon-32x32.png">
+</head>
+<body>
+<header>
+    <article class="logo">
+        <img src="styles/img/logo.jpg" alt="logo">
+    </article>
+    <h1>Bienvenida</h1>
+    <input TYPE="text" CLASS="input-nav">
+</header>
+<main>
+    <aside class="subcategorias-content">
+        <section class="categoria-content">
+            <a class="enlace-bloque-content" href="index.php?page=1">
+                <article class="imagen-content">
+                    <img src="styles/img/psicodelico.jpg" alt="Imagen1">
+                </article>
+                <article class="testo-content">
+                    <h1>Titulo 1</h1>
+                    <p>Parrafo de texto texto texto texto texto texto texto
+                        texto texto texto texto texto texto texto texto texto texto texto texto.
+                    </p>
+                </article>
+            </a>
+        </section>
+    </aside>
+
+
+
+</main>    <footer>
+    <section class="footer-section">
+        <h2>Médicos del Mundo España</h2>
+        <p>Conde de Vilches, 15 · 28028, Madrid</p>
+        <p>Lunes a viernes: 8:00 - 20:00</p>
+        <p>
+            Tel: <a href="tel:+34915436033">91 543 60 33</a> ·
+            Email: <a href="mailto:informacion@medicosdelmundo.org">informacion@medicosdelmundo.org</a>
+        </p>
+    </section>
+</footer></body>
+</html>
