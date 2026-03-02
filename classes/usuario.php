6a3da33564c224817064e3b51d708ddec0c118db
handresd-cyber <handresd@campusdigitalfp.com>
2026-03-02 13:27:36 +0100

    Add files via upload

--- classes/usuario.php
+++ classes/usuario.php
@@ -14,8 +14,30 @@ class Usuario extends Rol
         $this->id_rol = $id_rol;
 
 }
-public function verificarsesion(){
+public function set $titulo{
         return true;
+    }
+    public function setNombre($nombre){
+        $titulo->set($nombre);
+    }
+    public function setEmail($email)  {
+        $titulo->set($email);
+    }
+    public function setPassword($password){
+        $titulo->set($password);
+    }
+    public function setIdUsuario($id_usuario){
+        $titulo->set($id_usuario);
+    }
+    public function setIdMadre($id_madre){
+        $titulo->set($id_madre);
+    }
+    public function setRol($id_rol){
+        $titulo->set($id_rol);
+    }
+    public function setTitulo($titulo){
+        $titulo->set($titulo);
+
 }
 public function iniciarsesion(){
         return $sesion
