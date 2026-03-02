bfec36b99af813355484becb9ecc2f77bd3d1f72
handresd-cyber <handresd@campusdigitalfp.com>
2026-03-02 13:41:16 +0100

    Add files via upload
    getters y setters añadidos

--- classes/rol.php
+++ classes/rol.php
@@ -6,7 +6,48 @@ class Rol{
         $this->id_rol=$id_rol;
         $this->nombre=$nombre;
     }
-    public function listarroles(){
-        return $rol
+    public function getTitulo(){
+        return $this->titulo;
     }
+
+
+    public function getNombre(){
+        return $this->nombre;
+    }
+
+
+
+
+    public function getIdRol(){
+        return $this->id_rol;
+    }
+    public function setIdRol($id_rol){
+        $this->id_rol=$id_rol;
+    }
+    public function getId(){
+        return $this->id_rol;
+
+    }
+    public function getRol(){
+        return $this->nombre;
+    }
+    //GETTERS
+    public function setRol($rol){
+    $this->nombre=$rol;
+    return $this;
+    public function setIDRol($id_rol){
+        $this->id_rol=$id_rol;
+
+    }
+    public function setNombre($nombre){
+        $this->nombre=$nombre;
+
+    }
+    public function setTitulo($titulo){
+        $this->titulo=$titulo;
+
+    }
+    public function setID($id_bloque){
+        $this->id_bloque=$id_bloque;
+
 }
