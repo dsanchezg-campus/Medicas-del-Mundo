66750f6d04ccc232872fa7a6c2433b05f0e3057f
dsanchezg-campus <dsanchezg@campusdigitalfp.com>
2026-03-02 13:45:35 +0100

    getters y setter en rol

--- classes/rol.php
+++ classes/rol.php
@@ -1,53 +1,23 @@
 <?php
-class Rol{
+class Rol {
     private $id_rol;
     private $nombre;
     public function __construct($id_rol,$nombre){
         $this->id_rol=$id_rol;
         $this->nombre=$nombre;
     }
-    public function getTitulo(){
-        return $this->titulo;
+    //GETTERS
+    public function getTdRol(){
+        return $this->id_rol;
     }
-
-
     public function getNombre(){
         return $this->nombre;
     }
-
-
-
-
-    public function getIdRol(){
-        return $this->id_rol;
-    }
+    //METODOS
     public function setIdRol($id_rol){
-        $this->id_rol=$id_rol;
-    }
-    public function getId(){
-        return $this->id_rol;
-
-    }
-    public function getRol(){
-        return $this->nombre;
-    }
-    //GETTERS
-    public function setRol($rol){
-    $this->nombre=$rol;
-    return $this;
-    public function setIDRol($id_rol){
-        $this->id_rol=$id_rol;
-
+    $this->id_rol=$id_rol;
     }
     public function setNombre($nombre){
         $this->nombre=$nombre;
-
     }
-    public function setTitulo($titulo){
-        $this->titulo=$titulo;
-
-    }
-    public function setID($id_bloque){
-        $this->id_bloque=$id_bloque;
-
 }
