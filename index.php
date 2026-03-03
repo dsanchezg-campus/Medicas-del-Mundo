c2e59d031af4817ee39076546253e3087c9359d0
dsanchezg-campus <dsanchezg@campusdigitalfp.com>
2026-03-03 09:51:07 +0100

    arreglo cabecera con lista

--- index.php
+++ index.php
@@ -13,24 +13,24 @@
 </head>
 <body>
     <header>
-    <ul>
-        <li>
-        <article class="logo">
-            <a href="https://www.medicosdelmundo.org/" class="enlace-medicos">
-                <img src="styles/img/logo.png" alt="logo">
-            <a>
-        </article>
-        </li>
-        <li>
-        <h1>Bienvenida</h1>
-        </li>
-        <li>
-        <form method="POST" action="" class="form-cabecera">
-            <input type="text" class="input-nav" placeholder="Buscar">
-            <input type="submit" class="boton-invisible" value="Buscar">
-        </form>
-        </li>
-    </ul>
+        <ul class="lista-nav">
+            <li class="linea-nav">
+                <article class="logo">
+                    <a href="https://www.medicosdelmundo.org/" class="enlace-medicos">
+                        <img src="styles/img/logo.png" alt="logo">
+                    </a>
+                </article>
+            </li>
+            <li class="linea-nav">
+                <h1>Bienvenida</h1>
+            </li>
+            <li class="linea-nav">
+                <form method="POST" action="" class="form-cabecera">
+                    <input type="text" class="input-nav" placeholder="Buscar">
+                    <input type="submit" class="boton-invisible" value="Buscar">
+                </form>
+            </li>
+        </ul>
     </header>
     <main>
         <section class="categoria">
