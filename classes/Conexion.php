0689d7b5547ab2c0a0098c26a4f8b6136c8374d2
dsanchezg-campus <dsanchezg@campusdigitalfp.com>
2026-03-19 09:21:20 +0100

    clase conexion

---
+++ classes/Conexion.php
@@ -0,0 +1,23 @@
+<?php
+class Database
+{
+    private $servername;
+    private $username;
+    private$password;
+    private $dbname;
+    public function __construct(){
+        $this->servername = "localhost";
+        $this->username = "root";
+        $this->password = "";
+        $this->dbname = "bloque";
+    }
+    public function conectar(){
+        try {
+            $conn = new PDO("mysql:host=$this->servername;dbname=,$this->dbname", $this->username, $this->password);
+            // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }catch (PDOException $e){
+            echo "Connection failed: " . $e->getMessage();
+        }
+    }
+}
