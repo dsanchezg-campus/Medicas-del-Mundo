753a663c4dfb71f70673dc8fe07b393b92493530
dsanchezg-campus <dsanchezg@campusdigitalfp.com>
2026-03-19 09:32:34 +0100

    conexiones

--- classes/Conexion.php
+++ classes/Conexion.php
@@ -16,6 +16,7 @@ class Database
             $conn = new PDO("mysql:host=$this->servername;dbname=,$this->dbname", $this->username, $this->password);
             // set the PDO error mode to exception
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $conn;
         }catch (PDOException $e){
             echo "Connection failed: " . $e->getMessage();
         }
