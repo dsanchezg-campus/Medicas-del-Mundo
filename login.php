f1b25b4b8987ce1b8893411a03186c0ac62e2416
dsanchezg-campus <dsanchezg@campusdigitalfp.com>
2026-04-16 10:06:09 +0200

    cerrar sesion movido al controlador

--- login.php
+++ login.php
@@ -4,23 +4,6 @@ require_once "classes/Usuario.php";
 require_once "classes/DB.php";
 
 session_start();
-// para cuando te redirijan para cerrar la sesion, elimina la sesión
-Usuario::CerrarSesion();
-// Verificar si se envió el formulario de login
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["usuario"], $_POST["password"])) {
-    $usuario = htmlspecialchars($_POST["usuario"]);
-    $password = htmlspecialchars($_POST["password"]);
-    // Intentar iniciar sesión con las credenciales proporcionadas
-    // Guarda la usuaria en SESSION['usuaria']
-    if (Usuario::InicioSesion($usuario, $password)){
-        // Redirigir a la página según el rol del usuario
-        header("Location:".$_SESSION['usuaria']->getRol()."/index.php");
-        exit;
-    } else{
-        // Mostrar mensaje de error si las credenciales son incorrectas
-        $error = "Credenciales incorrectas";
-    }
-}
 ?>
 <!DOCTYPE html>
 <!-- Página de inicio de sesión para usuarios -->
