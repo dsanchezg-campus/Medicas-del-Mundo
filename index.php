6c6a97929dec303e2afdb6948ceb7481eb013cb0
dsanchezg-campus <dsanchezg@campusdigitalfp.com>
2026-04-16 11:20:50 +0200

    header cambiado en index.php y login.php

--- index.php
+++ index.php
@@ -18,27 +18,9 @@ require_once "Classes/Bloque.php";
     <link rel="icon" type="image/png" sizes="32x32" href="https://www.medicosdelmundo.org/app/themes/mdm/library/medias/favicon/favicon-32x32.png">
 </head>
 <body>
-<!-- Cabecera del sitio con logo, título y barra de búsqueda -->
-    <header>
-        <ul class="lista-nav">
-            <li class="linea-nav">
-                <article class="logo">
-                    <a href="https://www.medicosdelmundo.org/" class="enlace-medicos">
-                        <img src="styles/img/logo.png" alt="logo">
-                    </a>
-                </article>
-            </li>
-            <li class="linea-nav">
-                <h1>Bienvenida</h1>
-            </li>
-            <li class="linea-nav">
-                <form method="POST" action="" class="form-cabecera">
-                    <input type="text" class="input-nav" placeholder="Buscar">
-                    <input type="submit" class="boton-invisible" value="Buscar">
-                </form>
-            </li>
-        </ul>
-    </header>
+<?php
+include_once "header.php";
+?>
     <main>
         <!-- Contenido principal: muestra categorías o subcategorías y bloques de contenido según el parámetro 'page' -->
         <?php
--- login.php
+++ login.php
@@ -17,27 +17,9 @@ session_start();
     <link rel="icon" type="image/png" sizes="32x32" href="https://www.medicosdelmundo.org/app/themes/mdm/library/medias/favicon/favicon-32x32.png">
 </head>
 <body>
-<!-- Cabecera con logo y barra de búsqueda -->
-    <header>
-        <ul class="lista-nav">
-            <li class="linea-nav">
-                <article class="logo">
-                    <a href="https://www.medicosdelmundo.org/" class="enlace-medicos">
-                        <img src="styles/img/logo.png" alt="logo">
-                    </a>
-                </article>
-            </li>
-            <li class="linea-nav">
-                <h1>Bienvenida</h1>
-            </li>
-            <li class="linea-nav">
-                <form method="POST" action="" class="form-cabecera">
-                    <input type="text" class="input-nav" placeholder="Buscar">
-                    <input type="submit" class="boton-invisible" value="Buscar">
-                </form>
-            </li>
-        </ul>
-    </header>
+<?php
+include_once "header.php";
+?>
 <!-- Contenido principal con formulario de login -->
     <main class="login-container">
         <article class="login-section">
