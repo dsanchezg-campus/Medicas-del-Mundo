320823bfbd7b1db8acf9e39092b57f8f9810fae6
dsanchezg-campus <dsanchezg@campusdigitalfp.com>
2026-04-16 10:10:34 +0200

    creado controlador para iniciar sesion

---
+++ controladores/inicio_sesion.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+// Verificar si se envió el formulario de login
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["usuario"], $_POST["password"])) {
+    $usuario = htmlspecialchars($_POST["usuario"]);
+    $password = htmlspecialchars($_POST["password"]);
+    // Intentar iniciar sesión con las credenciales proporcionadas
+    // Guarda la usuaria en SESSION['usuaria']
+    if (Usuario::InicioSesion($usuario, $password)){
+        // Redirigir a la página según el rol del usuario
+        header("Location:".$_SESSION['usuaria']->getRol()."/index.php");
+        exit;
+    } else{
+        // Mostrar mensaje de error si las credenciales son incorrectas
+        $error = "Credenciales incorrectas";
+        header("Location: ../login.php?error=".$error);
+        exit();
+    }
+} else{
+    $error = "introduzca los datos";
+    header("Location: ../login.php?error=".$error);
+    exit();
+}
