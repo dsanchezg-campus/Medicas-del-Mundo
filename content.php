1c17b2fb63dd9c23336f38cf4caffca89b5353ec
dsanchezg-campus <dsanchezg@campusdigitalfp.com>
2026-04-16 11:19:29 +0200

    header separado

--- content.php
+++ content.php
@@ -18,27 +18,9 @@ require_once "classes/Contenido.php";
     <link rel="icon" type="image/png" sizes="32x32" href="https://www.medicosdelmundo.org/app/themes/mdm/library/medias/favicon/favicon-32x32.png">
 </head>
 <body>
-<!-- Cabecera con logo y barra de búsqueda -->
-    <header>
-        <ul class="lista-nav">
-            <li class="linea-nav">
-                <article class="logo">
-                    <a href="https://www.medicosdelmundo.org/" class="enlace-medicos">
-                        <img src="styles/img/logo.png" alt="logo">
-                    </a>
-                </article>
-            </li>
-            <li class="linea-nav">
-                <h1>Bienvenida</h1>
-            </li>
-            <li class="linea-nav">
-                <form method="POST" action="" class="form-cabecera">
-                    <input type="text" class="input-nav" placeholder="Buscar">
-                    <input type="submit" class="boton-invisible" value="Buscar">
-                </form>
-            </li>
-        </ul>
-    </header>
+<?php
+include "header.php";
+?>
 <!-- Contenido principal dividido en aside para bloques relacionados y sección para el contenido detallado -->
     <main>
         <aside class="subcategorias-content">
---
+++ header.php
@@ -0,0 +1,21 @@
+<!-- Cabecera con logo y barra de búsqueda -->
+    <header>
+        <ul class="lista-nav">
+            <li class="linea-nav">
+                <article class="logo">
+                    <a href="https://www.medicosdelmundo.org/" class="enlace-medicos">
+                        <img src="styles/img/logo.png" alt="logo">
+                    </a>
+                </article>
+            </li>
+            <li class="linea-nav">
+                <h1>Bienvenida</h1>
+            </li>
+            <li class="linea-nav">
+                <form method="POST" action="" class="form-cabecera">
+                    <input type="text" class="input-nav" placeholder="Buscar">
+                    <input type="submit" class="boton-invisible" value="Buscar">
+                </form>
+            </li>
+        </ul>
+    </header>
