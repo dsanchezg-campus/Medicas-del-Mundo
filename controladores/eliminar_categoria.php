e06413c8f7f0a4f3d05a9e9042880038ec62c588
dsanchezg-campus <dsanchezg@campusdigitalfp.com>
2026-04-16 09:27:43 +0200

    solucionado llamada funcion incorrecta

--- controladores/eliminar_categoria.php
+++ controladores/eliminar_categoria.php
@@ -1,5 +1,5 @@
 <?php
-if ($_SESSION['usuaria']->ControlUsuariaEditora || $_SESSION['usuaria']->ControlUsuariaAdmin()) {
+if ($_SESSION['usuaria']->ControlUsuariaEditora() || $_SESSION['usuaria']->ControlUsuariaAdmin()) {
     if ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['categoria'])) {
         Categoria::EliminarCategoria($_GET["categoria"]);
     } elseif ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['contenido'])) {
