48f7b7a1c60ac1445b95e39c6aeb24b9818b1724
dsanchezg-campus <dsanchezg@campusdigitalfp.com>
2026-04-17 09:50:06 +0200

    creado formulario de edición de editora

--- admin/editar_editora.php
+++ admin/editar_editora.php
@@ -24,3 +24,37 @@ require_once "../header.php";
         <?php
     }
     ?>
+    <article class="anadir-contenido">
+        <form action="" method="post" class="form-anadir">
+                <?php
+                $usuaria = Usuario::getUsuaria($_GET["id_usuario"]);
+                ?>
+
+            <label for="nombre"><?php echo $usuaria->getRol().": ". $usuaria->getNombre(); ?> </label>
+            <input type="text" id="nombre" name="nombre" placeholder="Cambiar Nombre">
+
+            <label for="email">Email: <?php echo $usuaria->getEmail();?> </label>
+            <input type="text" id="email" name="email" placeholder="Cambiar Email" accept="%@email.com">
+
+            <label for="password">Cambiar contraseña: </label>
+            <input type="password" id="password" name="password[]" placeholder="Introduzca nueva contraseña">
+            <input type="password" id="password" name="password[]" placeholder="Repite la contraseña">
+            <button type="submit">Editar usuaria</button>
+        </form>
+    </article>
+</main>
+<footer>
+    <section class="footer-section">
+        <h2>Médicos del Mundo España</h2>
+        <p>Conde de Vilches, 15 · 28028, Madrid</p>
+        <p>Lunes a viernes: 8:00 - 20:00</p>
+        <p>
+            Tel: <a href="tel:+34915436033">91 543 60 33</a> ·
+            Email: <a href="mailto:informacion@medicosdelmundo.org">informacion@medicosdelmundo.org</a>
+        </p>
+        <p><a href="login.php">Iniciar Sesion</a></p>
+    </section>
+</footer>
+<a href="../admin/index.php" class="volver-inicio"><img src="../styles/img/casita.png" alt="regresa a inicio"></a>
+</body>
+</html>
